2afabf1dad0f1c817fb1d221b33e2cb4820c3e87
Marcus Maziero <marcus.maziero@outlook.com>
2022-11-08 18:56:44 -0300

    Que haja luz

---
+++ src/main/java/com/xpto/vitrine/VitrineApplication.java
@@ -0,0 +1,13 @@
+package com.xpto.vitrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitrineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpto/vitrine/VitrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpto.vitrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
